441459f0bde2ebbfede02b16ffc95b4f7a15e7af
Jared Whiklo <jwhiklo@gmail.com>
2017-11-14 13:57:24 -0600

    Add more documentation and some debugging

--- tests/support_scripts/install_karaf.sh
+++ tests/support_scripts/install_karaf.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+echo "Downloading Apache Karaf version: ${KARAF_VERSION}"
 wget -q -O "$HOME/apache-karaf-${KARAF_VERSION}.tar.gz" "http://archive.apache.org/dist/karaf/$KARAF_VERSION/apache-karaf-${KARAF_VERSION}.tar.gz"
+echo "done"
 
 cd "$HOME"
+echo "Extracting Karaf"
 tar xf "$HOME/apache-karaf-${KARAF_VERSION}.tar.gz"
-mv apache-karaf-"$KARAF_VERSION" /opt
-
-ln -s "/opt/apache-karaf-$KARAF_VERSION" $KARAF_DIR
+echo "done"
+echo "Creating symlink for Karaf"
+ln -s "$HOME/apache-karaf-$KARAF_VERSION" $KARAF_DIR
+echo "done"
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+echo "Starting Karaf"
 $KARAF_DIR/bin/start
 sleep 60
+echo "done"
